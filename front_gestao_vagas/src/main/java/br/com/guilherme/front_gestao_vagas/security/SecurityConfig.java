737bc09d92e0a07f870cd8171f249d3e03282cc4
Guilherme <guilhearthur@hotmail.com>
2025-10-12 14:53:06 -0300

    security profile

--- front_gestao_vagas/src/main/java/br/com/guilherme/front_gestao_vagas/security/SecurityConfig.java
+++ front_gestao_vagas/src/main/java/br/com/guilherme/front_gestao_vagas/security/SecurityConfig.java
@@ -15,7 +15,8 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("/candidate/login").permitAll();
             auth.anyRequest().authenticated();
-        });
+        }).formLogin(form -> form.loginPage("/candidate/login"));
+
         return http.build();
     }
 }
